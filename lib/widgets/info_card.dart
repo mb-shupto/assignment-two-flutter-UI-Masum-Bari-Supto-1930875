c8c70200fd05df0c6c3a5407be8447ee6590478e
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-11-29 05:39:55 +0600

    created info card widget to reuse in profile page

---
+++ lib/widgets/info_card.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class InfoCard extends StatelessWidget {
+  final String title;
+  final String value;
+  final IconData icon;
+  final Color iconColor;
+
+  const InfoCard({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.icon,
+    this.iconColor = const Color(0xFF5B46EB),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+
+      child: Row(
+        children: [
+
+          Icon(icon, color: iconColor, size: 20),
+          const SizedBox(width: 16),
+
+          Expanded(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title,
+              style: const TextStyle(
+                fontSize: 13,
+                color: Colors.grey
+              ),
+              ),
+
+              const SizedBox(height: 4),
+              Text(value,style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+              ),
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
