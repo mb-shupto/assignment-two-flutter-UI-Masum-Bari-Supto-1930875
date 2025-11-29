657e00de3367ffc19bb378c1fe836fad1e05c484
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-11-29 21:14:48 +0600

    update main entry point to use MainScreen instead of HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:assignment_two_flutter/screens/home_page.dart';
+import 'package:assignment_two_flutter/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(),
+      home: const MainScreen(),
     );
   }
 }
