106f3ec40bff02d0f344f22a8a65c5b91ea254e1
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-11-29 06:50:56 +0600

    initially designed profile page

---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:assignment_two_flutter/widgets/info_card.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  int _currentIndex = 3;
+  static const Color primaryPurple = Color(0xFF5B46EB);
+
+  static const String name = "STUDENT NAME (YOUR NAME HERE)";
+  static const String studentID = "S12345 (YOUR ID HERE)";
+  static const String email = "student.name@iub.edu (YOUR IUB EMAIL HERE)";
+//   static const String bio = """
+// I'm currently focusing on my final year, balancing studies with building side projects.
+// I believe financial health is key to academic success. I love hiking on weekends and planning my next big travel adventure!
+// (Note: Students should replace the text above with their own description here!)
+//   """;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+
+      body: SafeArea(child: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+
+            const SizedBox(
+                height: 30
+            ),
+
+            const Text("User Profile",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+                textAlign: TextAlign.center),
+
+
+            const SizedBox(
+              height: 20),
+            
+            CircleAvatar(
+              radius: 60,
+              backgroundColor: const Color(0xFF5B46EB),
+              child: const Text("SN",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              ),
+            ),
+
+
+            const SizedBox(height: 40,),
+              InfoCard(title: 'Name', value: name),
+
+            const SizedBox(height: 15,),
+            InfoCard(title: 'Student ID', value: studentID),
+
+            const SizedBox(height: 15,),
+            InfoCard(title: "Email", value: email),
+
+            const SizedBox(height: 15,),
+            const Text("Bio / Story",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+
+            const SizedBox(height: 12,),
+
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: RichText(text: const TextSpan(
+                style: TextStyle(
+                  height: 1.7,
+                  fontSize: 15
+                ),
+                children: [
+                  TextSpan(
+                    text: "I'm currently focusing on my final year, balancing studies with building side projects. "
+                        "I believe financial health is key to academic success. "
+                        "I love hiking on weekends and planning my next big travel adventure!\n\n",
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+
+                  TextSpan(
+                    text: "(Note: Students should replace the text above with their own description here!)",
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              )),
+            )
+
+          ],
+        ),
+      )),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: primaryPurple,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_outlined),
+            label: 'Reports',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card_outlined),
+            label: 'Cards',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
