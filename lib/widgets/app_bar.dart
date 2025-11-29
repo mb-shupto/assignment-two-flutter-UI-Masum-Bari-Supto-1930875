79e41679975f06a96aa56f1828d5f5dde7ebaccd
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-11-29 20:43:17 +0600

    created appbar widget to reuse it in every pages.

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+
+  final String title;
+  const CustomAppBar({super.key, required this.title});
+
+  static const Color primaryPurple = Color(0xFF5B46EB);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  AppBar(
+      backgroundColor: Colors.white,
+      titleSpacing: 20,
+      toolbarHeight: 90,
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+
+          CircleAvatar(
+            radius: 24,
+            backgroundColor: primaryPurple,
+            child: const Text(
+              'S',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+
+
+          const SizedBox(width: 12),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text(
+                'Welcome back,',
+                style: TextStyle(color: Colors.grey, fontSize: 15),
+              ),
+              Text(
+                'STUDENT NAME',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.notifications_none_outlined, size: 28),
+          onPressed: () {},
+        ),
+        const SizedBox(width: 10),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(90);
+}
